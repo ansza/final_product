3b5fde3ad77e281f2509dc921bd3cb8ebe5fcc44
ansza <137867309+ansza@users.noreply.github.com>
2023-10-10 04:12:02 +0000

    change

--- Belilagi.py
+++ Belilagi.py
@@ -40,6 +40,5 @@ st.write("\nAssociation Rules:")
 # rules["antecedents"] = rules["antecedents"].map(lambda x:set(x))
 product_name = rules['antecedents'].unique()
 dropdown = st.selectbox('Select product to check', product_name)
-rules_selected = rules["consequents"].loc[rules["antecedents"] == dropdown]
-rules_selected = rules_selected.rename(columns={"consequents":"product_to_recommend"})
+rules_selected["product_to_recommend"] = rules["consequents"].loc[rules["antecedents"] == dropdown]
 st.write(rules_selected.map(lambda x:set(x)) )
